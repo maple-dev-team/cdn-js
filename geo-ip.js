f770d778eadb0fc5e39144cbc1f50c238910c7e1
Gabriel Fernandez <hotdesc@gmail.com>
2022-07-05 17:00:24 -0300

    fetch the api

--- geo-ip.js
+++ geo-ip.js
@@ -1,1 +1,7 @@
-console.log('hi');
+if (self.fetch) {
+  fetch("http://127.0.0.1:36867/api/get-store-url")
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log("data: ", data);
+    });
+}
