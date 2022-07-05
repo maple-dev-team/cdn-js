7dfe9d57d65baa986a59636874ca345cbecddf82
Gabriel Fernandez <hotdesc@gmail.com>
2022-07-05 18:31:26 -0300

    catch added

--- geo-ip.js
+++ geo-ip.js
@@ -3,5 +3,8 @@ if (self.fetch) {
     .then((resp) => resp.json())
     .then(function (data) {
       console.log("data: ", data);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 }
