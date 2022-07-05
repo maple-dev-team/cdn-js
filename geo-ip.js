7f407d2702963f1237a92acff5ed22ae551c90fd
Gabriel Fernandez <hotdesc@gmail.com>
2022-07-05 19:12:12 -0300

    fix port

--- geo-ip.js
+++ geo-ip.js
@@ -1,5 +1,5 @@
 if (self.fetch) {
-  fetch("http://127.0.0.1:36867/api/get-store-url")
+  fetch("http://127.0.0.1:35163/api/get-store-url")
     .then((resp) => resp.json())
     .then(function (data) {
       console.log("data: ", data);
