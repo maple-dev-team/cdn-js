d28665fd3422479b0a95f684050be692a66eaffe
Gabriel Fernandez <hotdesc@gmail.com>
2022-07-26 14:40:32 -0300

    ip api change

--- geo-ip.js
+++ geo-ip.js
@@ -1,2 +1,34 @@
 if (self.fetch) {
+  const isAdmin = window.location.href.includes("/admin");
+  const isRootUrl = window.location.href.includes("myshopify.com");
+  const isPreview = window.location.href.includes("shopifypreview");
+
+  if (!isPreview && !isAdmin && !isRootUrl) {
+    fetch("https://ipapi.co/json/", {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((resp) => resp.json())
+      .then(function (response) {
+        let url = "stlthvape.com";
+        switch (response.data.countryCode) {
+          case "UA":
+            url = "ua.stlthvape.com";
+            break;
+          case "PE":
+            url = "pe.stlthvape.com";
+            break;
+        }
+        const isTheRightOne = window.location.href.includes(url);
+        if (!isTheRightOne) {
+          window.location.href = "https://" + url;
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
 }
