35eadab7cee1ded29fa79a772fe4e23cdda7efd8
Gabriel Fernandez <hotdesc@gmail.com>
2022-07-26 12:11:11 -0300

    geoip url redirect fix

--- geo-ip.js
+++ geo-ip.js
@@ -1,22 +1,33 @@
 if (self.fetch) {
-  fetch("https://geo-ip.appforge.ca/api/get-store-url", {
+  fetch("http://ip-api.com/json/", {
     mode: "cors",
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
   })
     .then((resp) => resp.json())
-    .then(function (data) {
+    .then(function (response) {
+      let url = "stlthvape.com";
+      switch (response.data.countryCode) {
+        case "UA":
+          url = "ua.stlthvape.com";
+          break;
+        case "PE":
+          url = "pe.stlthvape.com";
+          break;
+      }
+
       const isPreview = window.location.href.includes("shopifypreview");
-      const isTheRightOne = window.location.href.includes(data.url);
+      const isTheRightOne = window.location.href.includes(url);
       const isAdmin = window.location.href.includes("/admin");
       const isRootUrl = window.location.href.includes("myshopify.com");
 
       if (!isPreview && !isAdmin && !isRootUrl && !isTheRightOne) {
-        window.location.href = "https://" + data.url;
+        window.location.href = "https://" + url;
       }
     })
     .catch(function (error) {
+      // handle error
       console.log(error);
     });
 }
