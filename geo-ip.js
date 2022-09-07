f823e28aabaac4417e8ce3edf1ea95958fb9a723
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-07 19:20:05 -0300

    css reset

--- geo-ip.js
+++ geo-ip.js
@@ -1,4 +1,6 @@
 import {html, css, LitElement} from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
+import { minireset } from 'https://cdn.jsdelivr.net/npm/minireset.css@0.0.7/minireset.css.lit.js';
+
 const privateOpen = Symbol('open');
 
 class OneDialog extends LitElement {
@@ -10,43 +12,108 @@ class OneDialog extends LitElement {
   }
   
   static get styles() {
-    return [css`
-      :host .wrapper {
-        opacity: 0;
-        transition: visibility 0s, opacity 0.25s ease-in;
-      }
-      :host .wrapper:not(.open) {
-        visibility: hidden;
-      }
-      :host .wrapper.open {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        height: 100vh;
-        position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-        opacity: 1;
-        visibility: visible;
-      }
-      :host .overlay {
-        background: rgba(0, 0, 0, 0.8);
-        height: 100%;
-        position: fixed;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-        width: 100%;
-      }
-      :host .dialog {
-        background: #000;
-        max-width: 600px;
-        padding: 1rem;
-        position: fixed;
-      }`];
+    return [
+      minireset,
+      css`
+      .wrapper {
+          opacity: 0;
+          transition: visibility 0s, opacity 0.25s ease-in;
+        }
+        .wrapper:not(.open) {
+          visibility: hidden;
+        }
+        .wrapper.open {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          height: 100vh;
+          position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+          opacity: 1;
+          visibility: visible;
+        }
+        .overlay {
+          background: rgba(0, 0, 0, 0.8);
+          height: 100%;
+          position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+          width: 100%;
+        }
+        .dialog {
+          background: #000;
+          max-width: 600px;
+          padding: 1rem;
+          position: fixed;
+        }
+
+        /* Jony css start */
+        .main-div {
+          font-family: 'Roboto';
+          font-size: 14px;
+          color:white; 
+          text-align: center; 
+          width:550px; 
+          margin:8px; 
+          display: flex; 
+          flex-direction: column;
+          padding-top: 20px;
+        }
+        .div-padding {
+            padding-top: 18px;
+        }
+        .div-padding-buttons {
+            padding-top: 24px;
+        }
+        .button-yes {
+            padding: 10px 10px;
+            font-size: 12px;
+            text-transform: uppercase;
+            color: #000;
+            background-color: #fff;
+            border: none;
+            border-radius: 8px;
+            outline: none;
+            text-decoration: none;
+        }
+        .button-yes:hover {
+            background-color: #0329d3;
+            box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
+            color: #fff;
+            xtransform: translateY(-7px);
+        }
+        .button-stay {
+            padding: 10px 10px;
+            font-size: 12px;
+            text-transform: uppercase;
+            color: #fff;
+            background-color: #000;
+            border: none;
+            border-radius: 8px;
+            outline: none;
+            text-decoration: none;
+        }
+        .button-stay:hover {
+            background-color: #0329d3;
+            box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
+            color: #fff;
+            xtransform: translateY(-7px);
+        }
+        .flex-center {
+            display:flex; 
+            flex-direction: row;
+            justify-content: center;
+        }
+        .flex-around{
+            display:flex; 
+            flex-direction: row;
+            justify-content: space-around;
+        }`];
   }
   
   firstUpdated() {
@@ -81,12 +148,8 @@ class OneDialog extends LitElement {
       </div>
     </div>`;
   }
-
-  createRenderRoot() {
-    return this;
-  }
-  
   get open() { return this[privateOpen]; }
+
   set open(isOpen) {
     this[privateOpen] = isOpen;
     const { activeElement } = document;
@@ -100,11 +163,13 @@ class OneDialog extends LitElement {
       document.removeEventListener('keydown', this._watchEscape);
     }
   }
+
   close() {
     this.open = false
     const closeEvent = new CustomEvent('dialog-closed');
     this.dispatchEvent(closeEvent);
   }
+
   _watchEscape(event) {
     if (event.key === 'Escape') {
         this.close();   
