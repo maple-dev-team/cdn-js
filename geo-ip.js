ec8fe9b9ef21efd64361f24c246fc271774b360e
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-07 13:06:29 -0300

    removing ts

--- geo-ip.js
+++ geo-ip.js
@@ -1,15 +1,20 @@
-import {html, css, LitElement} from 'https://cdn.jsdelivr.net/npm/lit-element@3.2.2/index.min.js';
-import {customElement, property} from 'https://cdn.jsdelivr.net/npm/lit-element@3.2.2/decorators.js';
-
-@customElement('simple-greeting')
+import {html, css, LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 export class SimpleGreeting extends LitElement {
   static styles = css`p { color: blue }`;
 
-  @property()
-  name = 'Somebody';
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Somebody';
+  }
 
   render() {
     return html`<p>Hello, ${this.name}!</p>`;
   }
 }
+customElements.define('simple-greeting', SimpleGreeting);
+
 document.getElementById("liquid-body").innerHTML+= '<simple-greeting name="World"></simple-greeting>'
