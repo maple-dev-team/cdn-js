a76fd573d27a0a28b84bd943a4f23cfe71642f77
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-07 18:15:33 -0300

    root added

--- geo-ip.js
+++ geo-ip.js
@@ -11,104 +11,106 @@ class OneDialog extends LitElement {
   
   static get styles() {
     return [css`
-      .wrapper {
-        opacity: 0;
-        transition: visibility 0s, opacity 0.25s ease-in;
-      }
-      .wrapper:not(.open) {
-        visibility: hidden;
-      }
-      .wrapper.open {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        height: 100vh;
-        position: fixed;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-        opacity: 1;
-        visibility: visible;
-      }
-      .overlay {
-        background: rgba(0, 0, 0, 0.8);
-        height: 100%;
-        position: fixed;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-        width: 100%;
-      }
-      .dialog {
-        background: #000;
-        max-width: 600px;
-        padding: 1rem;
-        position: fixed;
-      }
-
-      /* Jony css start */
-      .main-div {
-        font-family: 'Roboto';
-        font-size: 14px;
-        color:white; 
-        text-align: center; 
-        width:550px; 
-        margin:8px; 
-        display: flex; 
-        flex-direction: column;
-        padding-top: 20px;
-      }
-      .div-padding {
-          padding-top: 18px;
-      }
-      .div-padding-buttons {
-          padding-top: 24px;
-      }
-      .button-yes {
-          padding: 10px 10px;
-          font-size: 12px;
-          text-transform: uppercase;
-          color: #000;
-          background-color: #fff;
-          border: none;
-          border-radius: 8px;
-          outline: none;
-          text-decoration: none;
-      }
-      .button-yes:hover {
-          background-color: #0329d3;
-          box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
-          color: #fff;
-          xtransform: translateY(-7px);
-      }
-      .button-stay {
-          padding: 10px 10px;
-          font-size: 12px;
-          text-transform: uppercase;
-          color: #fff;
-          background-color: #000;
-          border: none;
-          border-radius: 8px;
-          outline: none;
-          text-decoration: none;
-      }
-      .button-stay:hover {
-          background-color: #0329d3;
-          box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
-          color: #fff;
-          xtransform: translateY(-7px);
-      }
-      .flex-center {
-          display:flex; 
-          flex-direction: row;
+      :root {
+        .wrapper {
+          opacity: 0;
+          transition: visibility 0s, opacity 0.25s ease-in;
+        }
+        .wrapper:not(.open) {
+          visibility: hidden;
+        }
+        .wrapper.open {
+          align-items: center;
+          display: flex;
           justify-content: center;
-      }
-      .flex-around{
-          display:flex; 
-          flex-direction: row;
-          justify-content: space-around;
+          height: 100vh;
+          position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+          opacity: 1;
+          visibility: visible;
+        }
+        .overlay {
+          background: rgba(0, 0, 0, 0.8);
+          height: 100%;
+          position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+          width: 100%;
+        }
+        .dialog {
+          background: #000;
+          max-width: 600px;
+          padding: 1rem;
+          position: fixed;
+        }
+
+        /* Jony css start */
+        .main-div {
+          font-family: 'Roboto';
+          font-size: 14px;
+          color:white; 
+          text-align: center; 
+          width:550px; 
+          margin:8px; 
+          display: flex; 
+          flex-direction: column;
+          padding-top: 20px;
+        }
+        .div-padding {
+            padding-top: 18px;
+        }
+        .div-padding-buttons {
+            padding-top: 24px;
+        }
+        .button-yes {
+            padding: 10px 10px;
+            font-size: 12px;
+            text-transform: uppercase;
+            color: #000;
+            background-color: #fff;
+            border: none;
+            border-radius: 8px;
+            outline: none;
+            text-decoration: none;
+        }
+        .button-yes:hover {
+            background-color: #0329d3;
+            box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
+            color: #fff;
+            xtransform: translateY(-7px);
+        }
+        .button-stay {
+            padding: 10px 10px;
+            font-size: 12px;
+            text-transform: uppercase;
+            color: #fff;
+            background-color: #000;
+            border: none;
+            border-radius: 8px;
+            outline: none;
+            text-decoration: none;
+        }
+        .button-stay:hover {
+            background-color: #0329d3;
+            box-shadow: 0px 15px 20px rgba(88, 125, 243, 0.4);
+            color: #fff;
+            xtransform: translateY(-7px);
+        }
+        .flex-center {
+            display:flex; 
+            flex-direction: row;
+            justify-content: center;
+        }
+        .flex-around{
+            display:flex; 
+            flex-direction: row;
+            justify-content: space-around;
+        }
       }`];
   }
   
@@ -159,17 +161,11 @@ class OneDialog extends LitElement {
       document.removeEventListener('keydown', this._watchEscape);
     }
   }
-
-  createRenderRoot() {
-    return this;
-  }
-  
   close() {
     this.open = false
     const closeEvent = new CustomEvent('dialog-closed');
     this.dispatchEvent(closeEvent);
   }
-  
   _watchEscape(event) {
     if (event.key === 'Escape') {
         this.close();   
