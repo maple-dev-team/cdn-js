52560b5dc50c2a2640e94c5e0eb83c33c103bfea
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-07 19:29:08 -0300

    add variables

--- geo-ip.js
+++ geo-ip.js
@@ -181,16 +181,58 @@ class OneDialog extends LitElement {
 customElements.define('one-dialog', OneDialog);
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var myDiv = document.createElement("div");
-  myDiv.id = 'one-dialog-popup';
-  myDiv.innerHTML = `
-    <one-dialog>
-      <span slot="country-content">
-        <img src="https://flagcdn.com/w40/ua.png" alt="Ukraine">
-        <span style="font-size: 36px;margin-left: 14px;">UKRAINE</span>
-      </span>
-    </one-dialog>`;
-  document.body.appendChild(myDiv);
-  document.querySelector('one-dialog').open = true;
+
+  if (self.fetch) {
+
+    const isAdmin = window.location.href.includes("/admin");
+    const isRootUrl = window.location.href.includes("myshopify.com");
+    const isPreview = window.location.href.includes("shopifypreview");
+    const previewBar = document.getElementById("preview-bar-iframe"); // some previews don't have myshopify.com or shopifypreview on the url, but have the preview bar added by Shopify
+
+    // Authorization: ApiKey YOUR_API_KEY
+    if (!isPreview && !isAdmin && !isRootUrl && !previewBar) {
+      fetch("https://geoip.appforge.ca/country/", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((resp) => resp.json())
+      .then(function (response) {
+        if(response.continent_code === 'SA' && response.country_iso_code !== 'BR'){
+          url = "https://pe.stlthvape.com";
+        }
+        else{
+          switch (response.country_iso_code) {
+            case "UA":
+              url = "https://ua.stlthvape.com";
+              break;
+            case "MA":
+              url = "https://ma.stlthvape.com";
+              break;
+          }
+        }
+        const isTheRightOne = window.location.href.includes(url);
+        if (!isTheRightOne) {
+          var myDiv = document.createElement("div");
+          myDiv.id = 'one-dialog-popup';
+          myDiv.innerHTML = `
+            <one-dialog>
+              <span slot="country-content">
+                <img src="https://flagcdn.com/w40/${response.country_iso_code}.png" alt="${response.country_name}">
+                <span style="font-size: 36px;margin-left: 14px;">${response.country_name}</span>
+              </span>
+            </one-dialog>`;
+          document.body.appendChild(myDiv);
+          document.querySelector('one-dialog').open = true;
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+    }
+  }
 });
 
