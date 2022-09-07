064a1f6c12164ce4d8945407dbbd585522e9205d
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-07 19:22:48 -0300

    minireset

--- geo-ip.js
+++ geo-ip.js
@@ -1,7 +1,8 @@
 import {html, css, LitElement} from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
-import { minireset } from 'https://cdn.jsdelivr.net/npm/minireset.css@0.0.7/minireset.css.lit.js';
 
 const privateOpen = Symbol('open');
+
+var minireset = css`/*! minireset.css v0.0.7 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}`;
 
 class OneDialog extends LitElement {
 
