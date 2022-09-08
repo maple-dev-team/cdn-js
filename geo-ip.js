07ea850704fb3785bcb41d7eeed1bc149d0e2e17
Gabriel Fernandez <hotdesc@gmail.com>
2022-09-08 17:16:13 -0300

    remove on load

--- geo-ip.js
+++ geo-ip.js
@@ -1,67 +1,64 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  if (self.fetch) {
-    const isAdmin = window.location.href.includes("/admin");
-    const isRootUrl = window.location.href.includes("myshopify.com");
-    const isPreview = window.location.href.includes("shopifypreview");
-    const previewBar = document.getElementById("preview-bar-iframe"); // some previews don't have myshopify.com or shopifypreview on the url, but have the preview bar added by Shopify
+if (self.fetch) {
+  const isAdmin = window.location.href.includes("/admin");
+  const isRootUrl = window.location.href.includes("myshopify.com");
+  const isPreview = window.location.href.includes("shopifypreview");
+  const previewBar = document.getElementById("preview-bar-iframe"); // some previews don't have myshopify.com or shopifypreview on the url, but have the preview bar added by Shopify
 
-    if (!isPreview && !isAdmin && !isRootUrl && !previewBar) {
-      fetch("https://geoip.appforge.ca/country/", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((resp) => resp.json())
-      .then(function (response) {
-        let currentSiteCountry = 'CANADA'
-        if(window.location.href.includes('https://ua.stlthvape.com')){
-          currentSiteCountry = 'UKRAINE'
+  if (!isPreview && !isAdmin && !isRootUrl && !previewBar) {
+    fetch("https://geoip.appforge.ca/country/", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((resp) => resp.json())
+    .then(function (response) {
+      let currentSiteCountry = 'CANADA'
+      if(window.location.href.includes('https://ua.stlthvape.com')){
+        currentSiteCountry = 'UKRAINE'
+      }
+      else if(window.location.href.includes('https://pe.stlthvape.com')){
+        currentSiteCountry = 'PERU'
+      }
+      else if(window.location.href.includes('https://pe.stlthvape.com')){
+        currentSiteCountry = 'MORROCO'
+      }
+      let urlTo = "https://stlthvape.com";
+      let countryTo = "CANADA";
+      if(response.continent_code === 'SA' && response.country_iso_code !== 'BR'){
+        urlTo = "https://pe.stlthvape.com";
+        countryTo = "PERU";
+      }
+      else{
+        switch (response.country_iso_code) {
+          case "UA":
+            urlTo = "https://ua.stlthvape.com";
+            countryTo = "UKRAINE";
+            break;
+          case "MA":
+            urlTo = "https://ma.stlthvape.com";
+            countryTo = "MORROCO";
+            break;
         }
-        else if(window.location.href.includes('https://pe.stlthvape.com')){
-          currentSiteCountry = 'PERU'
-        }
-        else if(window.location.href.includes('https://pe.stlthvape.com')){
-          currentSiteCountry = 'MORROCO'
-        }
-        let urlTo = "https://stlthvape.com";
-        let countryTo = "CANADA";
-        if(response.continent_code === 'SA' && response.country_iso_code !== 'BR'){
-          urlTo = "https://pe.stlthvape.com";
-          countryTo = "PERU";
-        }
-        else{
-          switch (response.country_iso_code) {
-            case "UA":
-              urlTo = "https://ua.stlthvape.com";
-              countryTo = "UKRAINE";
-              break;
-            case "MA":
-              urlTo = "https://ma.stlthvape.com";
-              countryTo = "MORROCO";
-              break;
-          }
-        }
-        const isTheRightOne = window.location.href.includes(urlTo);
-        if (!isTheRightOne) {
-          var myDiv = document.createElement("div");
-          myDiv.innerHTML = '<redirect-popup></redirect-popup>';
-          document.body.appendChild(myDiv);
+      }
+      const isTheRightOne = window.location.href.includes(urlTo);
+      if (!isTheRightOne) {
+        var myDiv = document.createElement("div");
+        myDiv.innerHTML = '<redirect-popup></redirect-popup>';
+        document.body.appendChild(myDiv);
 
-          document.querySelector('redirect-popup').flag = response.country_iso_code.toLowerCase()
-          document.querySelector('redirect-popup').countryFrom = currentSiteCountry
-          document.querySelector('redirect-popup').countryTo = countryTo
-          document.querySelector('redirect-popup').urlTo = urlTo
-          document.querySelector('redirect-popup').countryCurrent = response.country_name.toUpperCase()
-          document.querySelector('redirect-popup').open = true;
-        }
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-    }
+        document.querySelector('redirect-popup').flag = response.country_iso_code.toLowerCase()
+        document.querySelector('redirect-popup').countryFrom = currentSiteCountry
+        document.querySelector('redirect-popup').countryTo = countryTo
+        document.querySelector('redirect-popup').urlTo = urlTo
+        document.querySelector('redirect-popup').countryCurrent = response.country_name.toUpperCase()
+        document.querySelector('redirect-popup').open = true;
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
   }
-});
-
+}
